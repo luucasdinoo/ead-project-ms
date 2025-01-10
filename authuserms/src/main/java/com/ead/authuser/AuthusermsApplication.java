78c4b8bd237b0f9f7d1dd4701198b7cde21cdd32
Lucas Bernadino <luucasdinoo@gmail.com>
2025-01-09 21:55:06 -0300

    Initial Setup

---
+++ authuserms/src/main/java/com/ead/authuser/AuthusermsApplication.java
@@ -0,0 +1,13 @@
+package com.ead.authuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthusermsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthusermsApplication.class, args);
+    }
+
+}
---
+++ coursems/src/main/java/com/ead/course/CoursemsApplication.java
@@ -0,0 +1,13 @@
+package com.ead.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursemsApplication.class, args);
+    }
+
+}
